d54bc2c54af5659d6b6a0c291c4a2e6791975653
vadwwxz <vadwwxz@gmail.com>
2023-06-19 17:05:02 +0300

    Created buildSrc for dependencies, config, plugins, migrated build.gradle to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id(Plugins.Application.dependency)
+    id(Plugins.Android.dependency)
+    id(Plugins.Kapt.dependency)
+    id(Plugins.Parcelable.dependency)
+}
+
+android {
+    namespace = Config.appModulePackageName
+    compileSdk = Config.compileSdk
+
+    defaultConfig {
+        applicationId = Config.appModulePackageName
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        @Suppress("UnstableApiUsage")
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
+    }
+    kotlinOptions {
+        jvmTarget = Config.jvmTarget
+    }
+    viewBinding.enable = true
+}
+
+dependencies {
+    // Basic
+    implementation(Dependencies.Android.coreKtx)
+    implementation(Dependencies.Android.appCompat)
+    implementation(Dependencies.Android.constraintLayout)
+    implementation(Dependencies.Android.material)
+    implementation(Dependencies.Android.fragment)
+    // Unit Testing
+    testImplementation(Dependencies.UnitTesting.JUnit.core)
+    testImplementation(Dependencies.UnitTesting.KotlinX.coroutines)
+    // Ui Testing
+    androidTestImplementation(Dependencies.AndroidTesting.JUnit.core)
+    androidTestImplementation(Dependencies.AndroidTesting.JUnit.ui)
+    androidTestImplementation(Dependencies.AndroidTesting.Espresso.core)
+    androidTestImplementation(Dependencies.AndroidTesting.AndroidX.core)
+    androidTestImplementation(Dependencies.AndroidTesting.Barista.core)
+    // Navigation
+    implementation(Dependencies.Navigation.navigationFragment)
+    implementation(Dependencies.Navigation.navigationUi)
+    // Splash Screen
+    implementation(Dependencies.SplashScreen.main)
+    // Room
+    implementation(Dependencies.Room.core)
+    kapt(Dependencies.Room.compiler)
+}
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,24 @@
+object Plugins {
+    object Application {
+        const val dependency = "com.android.application"
+        const val version = "8.0.0"
+    }
+
+    object Library {
+        const val dependency = "com.android.library"
+        const val version = "8.0.0"
+    }
+
+    object Android {
+        const val dependency = "org.jetbrains.kotlin.android"
+        const val version = "1.8.10"
+    }
+
+    object Kapt {
+        const val dependency = "kotlin-kapt"
+    }
+
+    object Parcelable {
+        const val dependency = "kotlin-parcelize"
+    }
+}
